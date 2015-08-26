e4fb886f299d30de13651882655b90a20141085a
Santhosh Nageshwar <santhoshbabun@gmail.com>
2015-08-25 23:57:46 -0700

    Adding support for back button from player

--- app/src/main/java/com/santhoshn/spotifystreamer/PlayerActivity.java
+++ app/src/main/java/com/santhoshn/spotifystreamer/PlayerActivity.java
@@ -1,18 +1,23 @@
 package com.santhoshn.spotifystreamer;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
-
+import android.support.v7.app.ActionBar;
 import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
 
         int trackIndex = getIntent().getIntExtra(PlayerActivityFragment.TRACK_INDEX, 0);
         ArrayList playlist = getIntent().getParcelableArrayListExtra(PlayerActivityFragment.PLAY_LIST);
@@ -33,5 +38,16 @@ public class PlayerActivity extends AppCompatActivity {
         fragment.setArguments(arguments);
 
         getFragmentManager().beginTransaction().replace(R.id.player_container, fragment).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
