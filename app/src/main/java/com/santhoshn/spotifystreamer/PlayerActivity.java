b663b52ddc972e57b989cfb65de26d6ed8b460de
Santhosh Nageshwar <santhoshbabun@gmail.com>
2015-08-27 01:01:28 -0700

    Fixing the double call for PlayerFragments onCreateView

--- app/src/main/java/com/santhoshn/spotifystreamer/PlayerActivity.java
+++ app/src/main/java/com/santhoshn/spotifystreamer/PlayerActivity.java
@@ -10,6 +10,7 @@ import com.santhoshn.spotifystreamer.track.Track;
 import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
+    private final String PLAYER_FRAGMENT_TAG = "player";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,41 +29,33 @@ public class PlayerActivity extends AppCompatActivity {
         ArrayList<Track> playlist;
 
         //use the savedInstance values if available else read it from intent
-//        if (savedInstanceState != null) {
-//            trackIndex = savedInstanceState.getInt(PlayerActivityFragment.TRACK_INDEX, 0);
-//            playlist = savedInstanceState.getParcelableArrayList(PlayerActivityFragment.PLAY_LIST);
-//            seekUntil = savedInstanceState.getInt(PlayerActivityFragment.SEEK_TO, 0);
-//        } else {
-//            trackIndex = getIntent().getIntExtra(PlayerActivityFragment.TRACK_INDEX, 0);
-//            playlist = getIntent().getParcelableArrayListExtra(PlayerActivityFragment.PLAY_LIST);
-//            seekUntil = getIntent().getIntExtra(PlayerActivityFragment.SEEK_TO, 0);
-//        }
+        if (savedInstanceState == null) {
+            trackIndex = getIntent().getIntExtra(PlayerActivityFragment.TRACK_INDEX, 0);
+            playlist = getIntent().getParcelableArrayListExtra(PlayerActivityFragment.PLAY_LIST);
+            seekUntil = getIntent().getIntExtra(PlayerActivityFragment.SEEK_TO, 0);
+            PlayerActivityFragment fragment = (PlayerActivityFragment) getFragmentManager().findFragmentByTag(PLAYER_FRAGMENT_TAG);
+            if (fragment == null) {
+                fragment = new PlayerActivityFragment();
+            }
 
+            //Now create the arguments bundle and start the fragment.
+            Bundle arguments = new Bundle();
+            arguments.putInt(PlayerActivityFragment.TRACK_INDEX, trackIndex);
+            if (playlist != null) {
+                arguments.putParcelableArrayList(PlayerActivityFragment.PLAY_LIST, playlist);
+            }
 
-        trackIndex = getIntent().getIntExtra(PlayerActivityFragment.TRACK_INDEX, 0);
-        playlist = getIntent().getParcelableArrayListExtra(PlayerActivityFragment.PLAY_LIST);
-        seekUntil = getIntent().getIntExtra(PlayerActivityFragment.SEEK_TO, 0);
+            arguments.putInt(PlayerActivityFragment.SEEK_TO, seekUntil);
+            //if seekUntil is -1 its means just show whats playing.
+            if (seekUntil == PlayerActivityFragment.INVALID_INDEX) {
+                arguments.putBoolean(PlayerActivityFragment.IS_SHOW_NOW_PLAYING, true);
+            } else {
+                arguments.putBoolean(PlayerActivityFragment.IS_SHOW_NOW_PLAYING, false);
+            }
 
-
-        //Now create the arguments bundle and start the fragment.
-        Bundle arguments = new Bundle();
-        arguments.putInt(PlayerActivityFragment.TRACK_INDEX, trackIndex);
-        if (playlist != null) {
-            arguments.putParcelableArrayList(PlayerActivityFragment.PLAY_LIST, playlist);
+            fragment.setArguments(arguments);
+            getFragmentManager().beginTransaction().replace(R.id.player_container, fragment).commit();
         }
-
-        arguments.putInt(PlayerActivityFragment.SEEK_TO, seekUntil);
-        //if seekUntil is -1 its means just show whats playing.
-        if (seekUntil == PlayerActivityFragment.INVALID_INDEX) {
-            arguments.putBoolean(PlayerActivityFragment.IS_SHOW_NOW_PLAYING, true);
-        } else {
-            arguments.putBoolean(PlayerActivityFragment.IS_SHOW_NOW_PLAYING, false);
-        }
-
-        PlayerActivityFragment fragment = new PlayerActivityFragment();
-        fragment.setArguments(arguments);
-
-        getFragmentManager().beginTransaction().replace(R.id.player_container, fragment).commit();
     }
 
     @Override
